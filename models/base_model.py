fd80b8a7c37e88490114ff1831ebbdc2526f9371
mouhcine wildane <muhssenwildane@gmail.com>
2024-01-09 23:53:28 +0100

    ** kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-
+import uuid
+import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.today()
-        self.updated_at = self.created_at
+    def __init__(self, **kwargs):
+        if kwargs:
+            for (key, value) in kwargs.items():
+                if key != '__class__':
+                    if (key == "created_at" or key == "updated_at"):
+                        format_string = "%Y-%m-%dT%H:%M:%S.%f"
+                        value = datetime.datetime.strptime(value, format_string)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.today()
+            self.updated_at = self.created_at
     def __str__(self):
         return ("[BaseModel] ({}) {}".format(self.id, self.__dict__))
     def save(self):
