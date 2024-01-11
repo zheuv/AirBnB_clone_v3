ad2dcd21f1fd9fe700eff4908987bf447ef2e452
mouhcine wildane <muhssenwildane@gmail.com>
2024-01-11 16:46:08 +0100

    filestorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 import uuid
 import datetime
+from models import storage
 
 class BaseModel:
     def __init__(self, **kwargs):
+                """Initialization of BaseModel Class"""
+        self.id = str(uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
         if kwargs:
             for (key, value) in kwargs.items():
                 if key != '__class__':
@@ -12,13 +17,13 @@ class BaseModel:
                         value = datetime.datetime.strptime(value, format_string)
                     setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.today()
-            self.updated_at = self.created_at
+            storage.new()
     def __str__(self):
         return ("[BaseModel] ({}) {}".format(self.id, self.__dict__))
     def save(self):
         self.updated_at = datetime.datetime.today()
+        storage.save()
+
     def to_dict(self):
         dicti = dict()
         dicti["__class__"] = "BaseModel"
---
+++ models/engine/file_storage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = dict()
+    def all(self):
+        return FileStorage.__objects
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.name, obj.id)
+        FileStorage.__objects[key] = obj
+    def save(self):
+        serialized_objects = dict()
+        for key, obj in FileStorage.items() :
+            serialized_objects[key] = obj.to_dict()
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
+    def reload(self):
+        if os.path.exists(FileStorage.__file_path):
+            try :
+                with open(FileStorage.__file_path, 'r') as file):
+                    serialized_objects = json.load(file)
+                    for key, obj in serialized_objects.items():
+                        class_name, obj_id = key.split()
+                        obj_class = eval(class_name)
+                        obj_instance = obj_class(**data)
+                        FileStorage.__objects[key] = obj_instance
+            except Exception:
+                pass
+
+
+
