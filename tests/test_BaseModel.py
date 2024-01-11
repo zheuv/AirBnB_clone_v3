8a7e3fbd4141a9ac77ab34ec2fa813a0179f281e
mouhcine wildane <muhssenwildane@gmail.com>
2024-01-11 02:38:46 +0100

    shorttest

--- tests/test_BaseModel.py
+++ tests/test_BaseModel.py
@@ -10,36 +10,33 @@ import datetime
 import unittest
 
 class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.shared_model = BaseModel()
+        self.shared_model.name = "My_First_Model"
+        self.shared_model.my_number = 89
     def test_NoneKwargs(self):
-        my_model = BaseModel()
-        my_model.name = "My_First_Model"
-        my_model.my_number = 89
-        expected_output = f"[BaseModel] ({my_model.id}) {my_model.__dict__}"
-        actual_output = str(my_model)
+        expected_output = f"[BaseModel] ({self.shared_model.id}) {self.shared_model.__dict__}"
+        actual_output = str(self.shared_model)
         expected_output_dict = {
             '__class__': 'BaseModel',
-            'id': my_model.id,
-            'created_at': my_model.created_at.isoformat(),
-            'updated_at': my_model.updated_at.isoformat(),
-            'name': my_model.name,
-            'my_number': my_model.my_number
+            'id': self.shared_model.id,
+            'created_at': self.shared_model.created_at.isoformat(),
+            'updated_at': self.shared_model.updated_at.isoformat(),
+            'name': self.shared_model.name,
+            'my_number': self.shared_model.my_number
         }
-        actual_output_dict = my_model.to_dict()
+        actual_output_dict = self.shared_model.to_dict()
 
-        self.assertIsInstance(my_model.id, str)
-        self.assertIsInstance(my_model.created_at, datetime.datetime)
-        self.assertIsInstance(my_model.updated_at, datetime.datetime)
-        self.assertIsInstance(my_model.name, str)
-        self.assertIsInstance(my_model.my_number, int)
+        self.assertIsInstance(self.shared_model.id, str)
+        self.assertIsInstance(self.shared_model.created_at, datetime.datetime)
+        self.assertIsInstance(self.shared_model.updated_at, datetime.datetime)
+        self.assertIsInstance(self.shared_model.name, str)
+        self.assertIsInstance(self.shared_model.my_number, int)
         self.assertEqual(actual_output, expected_output)
         self.assertDictEqual(actual_output_dict, expected_output_dict)
     
     def test_kwargs(self):
-        my_model = BaseModel()
-        my_model.name = "My_First_Model"
-        my_model.my_number = 89
-        jsondict = my_model.to_dict()
-        mynew_model = BaseModel(**jsondict)
+        mynew_model = BaseModel(**self.shared_model.to_dict())
         expected_output = f"[BaseModel] ({mynew_model.id}) {mynew_model.__dict__}"
         actual_output = str(mynew_model)
         expected_output_dict = {
