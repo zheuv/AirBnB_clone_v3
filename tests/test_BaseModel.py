7a90c7b5fc511c82a0c0ee53a17ee930cf29acef
mouhcine wildane <muhssenwildane@gmail.com>
2024-01-11 02:16:04 +0100

    tsted kwargs

--- tests/test_BaseModel.py
+++ tests/test_BaseModel.py
@@ -25,6 +25,7 @@ class TestBaseModel(unittest.TestCase):
             'my_number': my_model.my_number
         }
         actual_output_dict = my_model.to_dict()
+
         self.assertIsInstance(my_model.id, str)
         self.assertIsInstance(my_model.created_at, datetime.datetime)
         self.assertIsInstance(my_model.updated_at, datetime.datetime)
@@ -32,6 +33,34 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(my_model.my_number, int)
         self.assertEqual(actual_output, expected_output)
         self.assertDictEqual(actual_output_dict, expected_output_dict)
+    
+    def test_kwargs(self):
+        my_model = BaseModel()
+        my_model.name = "My_First_Model"
+        my_model.my_number = 89
+        jsondict = my_model.to_dict()
+        mynew_model = BaseModel(**jsondict)
+        expected_output = f"[BaseModel] ({mynew_model.id}) {mynew_model.__dict__}"
+        actual_output = str(mynew_model)
+        expected_output_dict = {
+            '__class__': 'BaseModel',
+            'id': mynew_model.id,
+            'created_at': mynew_model.created_at.isoformat(),
+            'updated_at': mynew_model.updated_at.isoformat(),
+            'name': mynew_model.name,
+            'my_number': mynew_model.my_number
+        }
+        actual_output_dict = mynew_model.to_dict()
+
+        self.assertIsInstance(mynew_model.id, str)
+        self.assertIsInstance(mynew_model.created_at, datetime.datetime)
+        self.assertIsInstance(mynew_model.updated_at, datetime.datetime)
+        self.assertIsInstance(mynew_model.name, str)
+        self.assertIsInstance(mynew_model.my_number, int)
+        self.assertEqual(actual_output, expected_output)
+        self.assertDictEqual(actual_output_dict, expected_output_dict)
+
+
 
 if __name__ == '__main__':
     unittest.main()
