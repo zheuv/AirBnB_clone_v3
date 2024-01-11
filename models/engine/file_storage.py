cc84bb6d9a1b1e55e5ddfe8a1b6ef9604bbd9829
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-11 17:14:12 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,14 +21,13 @@ class FileStorage:
             json.dump(serialized_objects, file)
     def reload(self):
         if os.path.exists(FileStorage.__file_path):
-            try :
-                with open(FileStorage.__file_path, 'r') as file:
-                    serialized_objects = json.load(file)
-                    for key, obj in serialized_objects.items():
-                        class_name, obj_id = key.split()
-                        obj_class = eval(class_name)
-                        obj_instance = obj_class(**data)
-                        FileStorage.__objects[key] = obj_instance
+            with open(FileStorage.__file_path, 'r') as file:
+                serialized_objects = json.load(file)
+                for key, obj in serialized_objects.items():
+                    class_name, obj_id = key.split()
+                    obj_class = eval(class_name)
+                    obj_instance = obj_class(**data)
+                    FileStorage.__objects[key] = obj_instance
             
 
 
