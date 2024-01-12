13e74204503916e8d8dc51f024bf3c6dd854d707
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-12 13:50:29 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 
+""" Doc Here """
 import json
 import os
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     __file_path = "file.json"
