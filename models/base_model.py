3b58eef3f699ccc614bc004fb2e5779940aecd98
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-12 12:35:46 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
         else:
             models.storage.new(self)
     def __str__(self):
-        return ("[__class__.__name__] ({}) {}".format(self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__))
     def save(self):
         self.updated_at = datetime.datetime.today()
         models.storage.save()
