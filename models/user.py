a3a2bed42ad808c580add95460aa369d81d10c3f
mouhcine wildane <muhssenwildane@gmail.com>
2024-01-12 12:37:34 +0100

    user class

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User class that inherits from BaseModel."""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of User class."""
+        super().__init__(*args, **kwargs)
