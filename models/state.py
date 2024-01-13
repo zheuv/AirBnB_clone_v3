a9ed57400df54eae23f5626ed19e0f88c6bd1974
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-13 15:45:13 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
+""" a class to sophisticate console """
 from models.base_model import BaseModel
 
-
 class State(BaseModel):
+    """ State class which inherits from BaseModel """
     name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initialization of User class."""
+        """Initialization of State class."""
         super().__init__(*args, **kwargs)
