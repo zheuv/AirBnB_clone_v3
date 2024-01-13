f71f13b2ca2c5013cbc7475f28963aff00fdf840
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-13 15:50:22 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" Doc Here """
+""" Doc for file_storage """
 import json
 import os
 from models.base_model import BaseModel
@@ -12,20 +12,28 @@ from models.amenity import Amenity
 from models.review import Review
 
 class FileStorage:
+    """ FileStorage class """
     __file_path = "file.json"
     __objects = dict()
     def all(self):
+        """ returns the dictionary __objects """
         return FileStorage.__objects
     def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
     def save(self):
+        """ serializes __objects to the JSON file (path: __file_path) """
         serialized_objects = dict()
         for key, obj in FileStorage.__objects.items() :
             serialized_objects[key] = obj.to_dict()
         with open(FileStorage.__file_path, 'w') as file:
             json.dump(serialized_objects, file)
     def reload(self):
+        """ deserializes the JSON file to __objects 
+        (only if the JSON file (__file_path) exists ; 
+        otherwise, do nothing. If the file doesn’t exist, 
+        no exception should be raised) """
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r') as file:
                 serialized_objects = json.load(file)
