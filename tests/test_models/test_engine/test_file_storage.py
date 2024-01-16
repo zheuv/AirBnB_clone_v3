1b0c15bcbae54102f45c40d9f80f087e04bd3f5e
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-16 14:33:19 +0100

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -14,12 +14,9 @@ from models.review import Review
 class test_FileStorage(unittest.TestCase):
   fs = FileStorage()
   model = BaseModel()
+
   def test_type_objects(self):
-    self.assertEqual(type(self.fs.__objects), dict)
+    self.assertEqual(type(self.fs._FileStorage__objects), dict)
+
   def test_type_file_path(self):
-    self.assertEqual(type(self.fs.__file_path), str)
-  
-if __name__ == "__main__":
-    unittest.main()
-  
-  
+    self.assertEqual(type(self.fs._FileStorage__file_path), str)
