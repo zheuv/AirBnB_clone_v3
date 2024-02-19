66e1148b7d84ec6bdcf044d50794941c77735c28
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-02-19 20:57:04 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-""" Doc Here """
-from models.engine.file_storage import FileStorage
+""" the init file """
+import getenv
 
-storage = FileStorage()
-storage.reload()
+storage_type = getenv("HBNB_TYPE_STORAGE")
+if storage_type == "db":
+  from models.engine.db_storage import DBStorage
+  storage = DBStorage()
+  storage.reload()
+else:
+  from models.engine.file_storage import FileStorage
+  storage = FileStorage()
+  storage.reload()
