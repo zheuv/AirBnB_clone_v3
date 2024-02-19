14956b48579c82d32e1d69ad00d010942a34a615
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-02-19 18:05:40 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 """ a class to sophisticate console """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel):
-    """ State class which inherits from BaseModel """
-    name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initialization of State class."""
-        super().__init__(*args, **kwargs)
+class State(BaseModel, Base):
+    """ State class which inherits from BaseModel """from sqlalchemy.orm import relationship
+    __tablename__ = "states"
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade="all,delete", backref="state")
+    else:
+        name = ""
+        @property
+        def cities(self):
+            from models import storage
+            list = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id = self.id:
+                list.append(city)
+            return list
