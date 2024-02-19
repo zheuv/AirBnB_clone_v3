6ed9a8bb842b5b1a7071d6de01525c22f6897f6e
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-02-19 20:51:50 +0100

    Create db_storage.py

---
+++ models/engine/db_storage.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""This module defines a class to manage file storage for hbnb clone"""
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.base_model import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
+
+
+class DBStorage:
+  __engine = None
+  __session = None
+  def __init__(self):
+    """Function Docs"""
+    hb_user = getenv("HBNB_MYSQL_USER")
+    hb_pwd = getenv("HBNB_MYSQL_PWD")
+    hb_host = getenv("HBNB_MYSQL_HOST")
+    hb_db = getenv("HBNB_MYSQL_DB")
+    hb_env = getenv("HBNB_ENV")
+
+    self.__engine = create_engine(
+        f"mysql+mysqldb://{hb_user}:{hb_pwd}@{hb_host}/{hb_db}",
+        pool_pre_ping=True,
+    )
+
+    if hb_env == "test":
+      Base.metadata.drop_all(self.__engine)
+
+  def all(self, cls=None):
+    list = ["User", "Amenity", "Place", "City", "State", "Review"]
+    dicti = dict()
+    if cls is not None:
+      if cls in list:
+        rows = self.__session.query(cls).all()
+        for row in rows:
+          instance = "{}.{}".format(cls, row.id)
+          dicti [instance] = row
+      return dicti
+    else:
+      for class_name in list:
+        rows = self.__session.query(class_name).all()
+        for row in rows:
+          instance = "{}.{}".format(class_name, row.id)
+          dicti[instance] = row
+      return dicti
+
+  def new(self, obj):
+    if obj is not None:
+      self.__session.add(obj)
+  def save(self):
+    self.__session.commit()
+  def delete(self, obj):
+    if obj is not None:
+      self.__session.delete(obj)
+
+  def reload(self):
+      """ reload method """
+      Base.metadata.create_all(self.__engine)
+      Session = scoped_session(
+        sessionmaker(bind=self.__engine, expire_on_commit=False)
+      )
+      self.__session = Session()
+  
