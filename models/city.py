67adb110b54c0f31f8d534ba6117ac22ef3f6743
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-02-19 18:07:40 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -2,11 +2,17 @@
 """ a class to sophisticate console """
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
     """City class that inherits from BaseModel."""
     __tablename__ = "cities"
-    
-    state_id = Column(String(60), ForeignKey('states.id')
-    name = Column(String(128), nullable=False)
+    if storage_type == "db":
+        state_id = Column(String(60), ForeignKey('states.id')
+        name = Column(String(128), nullable=False)
+    else:
+        state_id = ""
+        name = ""
