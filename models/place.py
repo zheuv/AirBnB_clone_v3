6db7fd6fefaa984c34aaac5dafebd5486a52db31
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-02-20 17:07:22 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python3
 """ a class to sophisticate console """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """Place class that inherits from BaseModel."""
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
-
-    def __init__(self, *args, **kwargs):
-        """Initialization of Place class."""
-        super().__init__(*args, **kwargs)
+    __Tablename__ = "places"
+    if storage_type = "db":
+        city_id = Column(String(60), nullable=False, ForeignKey("cities.id"))
+        user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
