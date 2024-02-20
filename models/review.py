30973b26bbdd0e131d40005a032bdc6a057b0b04
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-02-20 19:52:25 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """ a class to sophisticate console """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, Text, ForeignKey
+from sqlalchemy.orm import relationship
 from os import getenv
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
+
 class Review(BaseModel, Base):
-    """User class that inherits from BaseModel."""
-    __tablename__ = "reviews"
+    """ Review classto store review information """
+    __tablename__ = 'reviews'
+
     if storage_type == "db":
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), nullable=False, ForeignKey("places.id"))
-        user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(Text(1024), nullable=False)
     else:
         place_id = ""
         user_id = ""
