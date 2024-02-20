a15392b8808d48c1d524f4da2af62871ac989653
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-02-20 17:33:09 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -2,7 +2,6 @@
 """ a class to sophisticate console """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey, Float
-from sqlalchemy.orm import relationship
 from os import getenv
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
@@ -10,10 +9,10 @@ storage_type = getenv("HBNB_TYPE_STORAGE")
 
 class Place(BaseModel, Base):
     """Place class that inherits from BaseModel."""
-    __Tablename__ = "places"
+    __tablename__ = "places"
     if storage_type == "db":
-        city_id = Column(String(60), nullable=False, ForeignKey("cities.id"))
-        user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
