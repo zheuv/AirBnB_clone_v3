e473e0e4193bddad74a73954abe4091275e672d9
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-02-21 15:19:38 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -12,7 +12,7 @@ class City(BaseModel, Base):
     """City class that inherits from BaseModel."""
     __tablename__ = "cities"
     if storage_type == "db":
-        state_id = Column(String(60), ForeignKey('states.id'))
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
         places = relationship("Place", cascade="all,delete", backref="cities")
     else:
